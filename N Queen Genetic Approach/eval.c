04ae1a25983f5a9bf4c3b2803b3c2d7495d6f340
masoudmousavi <mmousavi703@gmail,com>
2021-03-29 20:27:22 +0430

    Added another eval function

--- N Queen Genetic Approach/eval.c
+++ N Queen Genetic Approach/eval.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
 #include <math.h>    /* for pow(x, y) */
 #include "type.h"
+#include <stdlib.h>
 
 double decode(IPTR pj, int index, int size);
 double binToDec(int *chrom, int l);
 
+
+int compare(const void* a, const void* b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
 double eval(POPULATION *p, IPTR pj) 
      /* Called from gen.c and init.c */
 {
-    int GuardSum = 0;
-    int GuardMax = p->lchrom * (p->lchrom - 1) / 2;
-    for (int i = 1; i < p->lchrom; i++) {
-        GuardSum += GuardCount(pj, i,p->lchrom);
-    }
-    //printf("%d", GuardSum);
+    //int GuardSum = 0;
+    //int GuardMax = p->lchrom * (p->lchrom - 1) / 2;
+    //for (int i = 1; i < p->lchrom; i++) {
+    //    GuardSum += GuardCount(pj, i,p->lchrom);
+    //}
+    ////printf("%d", GuardSum);
 
-    return 1 - ((double)GuardSum / GuardMax);
-    //return (double)GuardMax - GuardSum;
+    //return 1 - ((double)GuardSum / GuardMax);
+    ////return (double)GuardMax - GuardSum;
+    return (double)__eval(p, pj);
+}
+
+
+double __eval(POPULATION* p, IPTR pj) {
+    int t1 = 0;
+    int t2 = 0;
+    
+    int len = p->lchrom;
+    int f1[25];
+    int f2[25];
+    for (int i = 0; i < len; i++) {
+        f1[i] = pj->chrom[i] - i;
+        f2[i] = 1 + len - pj->chrom[i] - i;
+    }
+    qsort(f1, len, sizeof(int), compare);
+    qsort(f2, len, sizeof(int), compare);
+    for (int i = 1; i < len; i++) {
+        if (f1[i] == f1[i - 1]) t1++;
+        if (f2[i] == f2[i - 1]) t2++;
+    }
+    return -t1 - t2;
 }
 
 double decode(IPTR pj, int index, int size)
