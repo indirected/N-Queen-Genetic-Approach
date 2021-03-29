b64deb5489e9696849785b6dfc5445570713e802
masoudmousavi <mmousavi703@gmail,com>
2021-03-29 23:46:39 +0430

    Updated tournament function

--- N Queen Genetic Approach/select.c
+++ N Queen Genetic Approach/select.c
@@ -30,9 +30,20 @@ int Tournoment(IPTR pop, int popsize, int lchrom ) {
         t1 = rnd(0, popsize - 1);
         t2 = rnd(0, popsize - 1);
     } while (t1 == t2);
+    int scaled_fitness[2];
 
-    int rand = (int)f_random() * (lchrom) * (lchrom - 1) / 2;
-    if (pop[t1].fitness > rand) return t1;
+    if (pop[t1].fitness > pop[t2].fitness) {
+        scaled_fitness[0] = 2/3;
+        scaled_fitness[1] = 1/3;
+
+    }
+    else {
+        scaled_fitness[1] = 2/3;
+        scaled_fitness[0] = 1/3;
+    }
+
+    double rand = f_random();
+    if (scaled_fitness[0] > rand) return t1;
     else return t2;
 }
 
